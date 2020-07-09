9d203d1dda96b18d57ebfdeb909e4e0a8ef36897
popofzk <847473488@qq.com>
2020-07-09 23:49:54 +0800

    页面适配性优化：添加不同浏览器适配，页面布局重新美化

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./assets/css/index.css";
+import Home from "./components/Home";
+
+import Result from "./components/Result";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Layout, Menu, Breadcrumb } from "antd";
+import { MailOutlined } from "@ant-design/icons";
+import { QqOutlined } from "@ant-design/icons";
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="page-wrap">
+        <Header>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1">
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            {/* <Menu.Item key="2">
+              <Link to="/Result">Result</Link>
+            </Menu.Item> */}
+            <Menu.Item key="3">
+              <a href={require("./assets/test1.XML")} download>
+                Download Test Case
+              </a>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <div className="page-content">
+          <Route exact path="/">
+            {" "}
+            <Home />
+          </Route>
+          <Route path="/result">
+            {" "}
+            <Result />
+          </Route>
+        </div>
+        <Footer className="page-footer">
+          <div className="footer-content">
+            <div className="footer-item one">
+              <p className="footer-logo">SmartCiteCon</p>
+              <div>
+                {/* 语义相关的引文上下文抽取工具 */}
+                <p>
+                  Semantically relevant citation context extraction tools
+                  <br />
+                  The user can complete the extraction of all semantically
+                  relevant citation contexts in SmartCiteCon, including explicit
+                  and implicit contexts
+                  <br />
+                  Integrated Grobid tool that allows users to process pdf or xml
+                  files
+                </p>
+                {/* 用户可以在
+              smartcite中完成对引文的显式上下文,隐式上下文等所有语义相关的引文上下文的抽取 */}
+                {/* <p>
+            
+            </p> */}
+                {/* 集成Grobid工具,用户可以对pdf格式或者xml文件进行相关处理 */}
+                {/* <p>
+            </p> */}
+              </div>
+            </div>
+            <div className="footer-item two">
+              <p style={{ fontSize: 20 }}>Contact Us</p>
+              <div className="footer-icon">
+                <div>
+                  <MailOutlined style={{ fontSize: 70, color: "#fff" }} />
+                </div>
+              </div>
+              <p>Email：chenruiguo@whu.edu.cn</p>
+            </div>
+          </div>
+        </Footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react'
+import '../assets/css/index.css'
+import { Upload, message, Button, Spin } from 'antd'
+import { UploadOutlined } from '@ant-design/icons'
+import { Input } from 'antd'
+import { SearchOutlined } from '@ant-design/icons'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { notification } from 'antd'
+// import axios from 'axios'
+import { useState } from 'react'
+import { useHistory } from "react-router-dom";
+const Home = ( ) => {
+  const file = []
+  const [fileName, setFileName]= useState('')
+  // const [userUploaded,setUserUploaded]= useState(false)
+  const [response, setResponse] = useState('')
+  const [isUploading, setUploading] = useState(false)
+  const onChange = ({ file }) => {
+    console.log('--正在上传的file--', file)
+    localStorage.setItem("uploadFileName", file.name);
+    setFileName(file.name)
+    // setUserUploaded(true)
+    if (file.status == 'uploading'){
+      setUploading(true);
+      localStorage.removeItem("fileName");
+      localStorage.removeItem("fileRes");
+    }
+  
+    
+  }
+  let history = useHistory();
+  const onSuccessUpload = (res) => {
+    console.log('--返回的结果--', res);
+    setUploading(false);
+    localStorage.setItem("fileName", localStorage.getItem("uploadFileName"));
+    setFileName(localStorage.getItem("uploadFileName"));
+    localStorage.setItem("fileRes", JSON.stringify(res));
+    if(res.code==500){
+      notification.error({
+            message: 'This file can not be processed, please upload another file',
+            duration: 15,
+            // description
+          })     
+    }else if(!isUploading){
+      history.push('/result');
+      notification.success({
+        message: 'Completed',
+        duration: 5,
+        // description
+      })     
+    }
+
+
+    // if (!isUploading){
+    //   history.push('/result');
+    //   notification.success({
+    //     message: 'Completed',
+    //     duration: 5,
+    //     // description
+    //   })     
+    // }
+  }
+  // 上传失败
+  const onFailUpload = () => {
+    setUploading(false);
+    notification.error({
+      message: 'Upload failed, you can refer to the test case in the top right corner and re-upload.',
+      duration: 20,
+      // description
+    })   
+  }
+    // const { fileList } = this.state
+    // const { file } = this.state
+    const props = {
+        name: 'file',//name得看接口需求，name与接口需要的name一致
+        // action: '/test/upload/file',//接口路径
+        action: 'http://114.55.101.144:8080/extract',//接口路径
+        // action: '/extract',
+        data: {file} ,//接口需要的参数，无参数可以不写
+        multiple: false,//支持多个文件
+        showUploadList: true,//展示文件列表
+        headers: {
+          // "Content-Type": "multipart/form-data"
+        },
+    }
+
+    //  
+    // let history = useHistory();
+    const submit=(  )=>{
+      if ( isUploading&&!localStorage.getItem("fileName")){
+        notification.error({
+          message: 'please wait patiently, the system is processing...',
+          duration: 8,         
+        })
+      } else if( !isUploading && !localStorage.getItem("uploadFileName")){
+        notification.error({
+          message: 'Please upload a pdf or xml file first',
+          duration: 8,     
+        })
+      }
+      else{
+     
+        // history.push('/result')
+        notification.success({
+          message: 'You have submitted',
+          duration: 5,
+          // description
+        })     
+      }   
+    }
+    
+    return (
+      <div className="component-wrap home">
+        <div>
+          <div className="smartcite">SmartCiteCon</div>
+          <div className="upload-wrap">
+            <div>
+            <Upload {...props}
+              fileList={file}
+              onChange={onChange}
+              onSuccess={onSuccessUpload}
+              onError={onFailUpload}
+            >
+              <Button className="uploadbutton" type="Link" shape="round" size="large">
+                <UploadOutlined /> {isUploading && <Spin></Spin>}
+                {isUploading && <span>Processing... Please wait a moment</span>}
+                {/* {!isUploading && <SearchOutlined />} */}
+                {!isUploading && <span>{fileName ==''?'Please select a pdf or xml file for citation context extraction':fileName}</span>}
+              </Button>
+            </Upload>
+            </div>
+            {/* <div> */}
+              {/* <Button type="default" size="large" disabled='true'>
+                  {isUploading && <Spin></Spin>}
+                  {isUploading && <span>Processing... Please wait a moment</span>}
+                  {!isUploading && <SearchOutlined />}
+                  {!isUploading && <span>{fileName ==''?'Please select a pdf or xml file for citation context extraction':fileName}</span>}
+              </Button> */}
+            {/* </div> */}
+          </div>
+        </div>
+      </div>
+    )
+  
+}
+
+export default Home
